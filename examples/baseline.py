f93e3a7e2e9fd9d8d34e36cafd2b7ccbf1dd4def
Walusus <marcinw.99.mw@gmail.com>
2022-02-13 18:45:04 +0100

    add baseline model example

--- examples/baseline.py
+++ examples/baseline.py
@@ -1,1 +1,14 @@
+import torchvision.io as io
+import pytorch_lightning as pl
+
 from nst.models import Baseline
+
+
+if __name__ == "__main__":
+    content_image = io.read_image("examples/images/content_image.jpg")
+    style_image = io.read_image("examples/images/style_image.jpg")
+
+    model = Baseline(content_image, style_image)
+
+    trainer = pl.Trainer(max_epochs=10)
+    trainer.fit(model)
