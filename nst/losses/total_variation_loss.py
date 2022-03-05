895e39b39c84526588184a3b30d2f8242011f046
KrzysiekDD <krzysiekd.navy@gmail.com>
2022-03-05 14:03:18 +0100

    add style loss functionality

--- nst/losses/total_variation_loss.py
+++ nst/losses/total_variation_loss.py
@@ -1,17 +1,20 @@
 import torch
 from typing import List
-from nst.models import Baseline
 
 
 class TotalVariationLoss:
     def __call__(self, image: torch.Tensor) -> torch.Tensor:
-        image = torch.sum(torch.abs(image[:, :, :, :-1] - image[:, :, :, 1:])) + torch.sum(
-            torch.abs(image[:, :, :-1, :] - image[:, :, 1:, :])
-        )
-        return image
         """
         Compute total variation loss of the input image.
 
         :param image: tensor representing input image with shape [batch, channels, height, width]
         :returns: vector of len = batch representing total variation loss
         """
+        batch, _, _, _ = image.size()
+
+        tv_loss = torch.sum(
+            torch.abs(image[:, :, :, :-1].view(batch, -1).sum(1) - image[:, :, :, 1:].view(batch, -1).sum(1))
+        ) + torch.sum(
+            torch.abs(image[:, :, :-1, :].view(batch, -1).sum(1) - image[:, :, 1:, :].view(batch, -1).sum(1))
+        )
+        return tv_loss
