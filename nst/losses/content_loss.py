1527e65fc30923e61d81224d87602ea43b2e130e
KrzysiekDD <krzysiekd.navy@gmail.com>
2022-03-05 12:55:53 +0100

    Fix content loss functionality

--- nst/losses/content_loss.py
+++ nst/losses/content_loss.py
@@ -1,6 +1,6 @@
 import torch
 from typing import List
-from nst.models import Baseline
+from torch.nn.functional import mse_loss
 
 
 class ContentLoss:
@@ -8,20 +8,22 @@ class ContentLoss:
         self,
         content_target_features: List[torch.Tensor],
     ):
-        self.content_target_features = content_target_features
         """
         Loss between content target feature maps and input image feature maps.
 
         :param content_target_features: list of conten target feature maps
         """
+        self.content_target_features = content_target_features
 
     def __call__(self, image_features: List[torch.Tensor]) -> torch.Tensor:
-        current_content_loss = torch.nn.MSELoss(reduction="mean")(self.content_target_features, image_features)
-        return current_content_loss
         """
         Compute and sum content loss for each feature map of the input image.
 
         :param image_features: list of feature maps of the input image
-        (feature map shape [batch, channels, height, width])
+            (feature map shape [batch, channels, height, width])
         :returns: vector of len = batch representing content loss
         """
+        sum_of_losses = torch.zeros(image_features[0].shape[0])
+        for index, feature_map in enumerate(image_features):
+            sum_of_losses += mse_loss(self.content_target_features[index], feature_map)
+        return sum_of_losses
