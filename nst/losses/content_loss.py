088237e5be4048c26be67c997011735ab06c4f3a
Walusus <marcinw.99.mw@gmail.com>
2022-03-16 13:39:18 +0100

    make losses functional

--- nst/losses/content_loss.py
+++ nst/losses/content_loss.py
@@ -4,32 +4,30 @@ from nst.losses.mse_batch_loss import MSEBatchLoss
 
 
 class ContentLoss:
-    def __init__(
-        self,
-        content_target_features: List[torch.Tensor],
-    ):
+    def __init__(self):
         """
         Loss between content target feature maps and input image feature maps.
-
-        :param content_target_features: list of conten target feature maps
         """
-        self._content_target_features = content_target_features
         self._loss = MSEBatchLoss()
 
-    def __call__(self, image_features: List[torch.Tensor]) -> Optional[torch.Tensor]:
+    def __call__(
+        self, input_features: List[torch.Tensor], target_features: List[torch.Tensor]
+    ) -> Optional[torch.Tensor]:
         """
         Compute and sum content loss for each feature map of the input image.
 
-        :param image_features: list of feature maps of the input image
+        :param input_features: list of feature maps of the input image
+            (feature map shape [batch, channels, height, width])
+        :param target_features: list of feature maps of the target image
             (feature map shape [batch, channels, height, width])
         :returns: vector of len = batch representing content loss or None if image_features is empty
         """
         loss = None
 
-        for input_features, target_features in zip(image_features, self._content_target_features):
+        for input_feature_map, target_feature_map in zip(input_features, target_features):
             if loss is None:
-                loss = self._loss(input_features, target_features)
+                loss = self._loss(input_feature_map, target_feature_map)
             else:
-                loss += self._loss(input_features, target_features)
+                loss += self._loss(input_feature_map, target_feature_map)
 
         return loss
--- nst/losses/style_loss.py
+++ nst/losses/style_loss.py
@@ -4,31 +4,38 @@ from nst.losses.mse_batch_loss import MSEBatchLoss
 
 
 class StyleLoss:
-    def __init__(self, style_target_features: List[torch.Tensor]):
+    def __init__(self):
         """
         Loss between style target feature maps and input image feature maps.
         Computed using gram matrix of the feature maps.
-
-        :param style_target_features: list of style target feature maps
         """
-        self._style_target_gram_matrices = []
-        for feature_map in style_target_features:
-            self._style_target_gram_matrices.append(self._gram_matrix(feature_map))
-
         self._loss = MSEBatchLoss()
 
-    def __call__(self, image_features: List[torch.Tensor]) -> Optional[torch.Tensor]:
+    def __call__(
+        self,
+        input_features: List[torch.Tensor],
+        target_features: List[torch.Tensor],
+        compute_target_gram: bool = False,
+    ) -> Optional[torch.Tensor]:
         """
         Compute and sum style loss for each feature map of the input image.
 
-        :param image_features: list of feature maps of the input image
+        :param input_features: list of feature maps of the input image
             (feature map shape [batch, channels, height, width])
+        :param target_features: list of feature maps of the target image
+            (feature map shape [batch, channels, height, width])
+        :param compute_target_gram: if false assume that list of target features contains gram matrices
         :returns: vector of len = batch representing style loss or None if image_features is empty
         """
         loss = None
 
-        for input_features, target_gram_matrix in zip(image_features, self._style_target_gram_matrices):
-            input_gram_matrix = self._gram_matrix(input_features)
+        for input_feature_map, target_feature_map in zip(input_features, target_features):
+            input_gram_matrix = self.gram_matrix(input_feature_map)
+
+            if compute_target_gram:
+                target_gram_matrix = self.gram_matrix(target_feature_map)
+            else:
+                target_gram_matrix = target_feature_map
 
             if loss is None:
                 loss = self._loss(input_gram_matrix, target_gram_matrix)
@@ -36,11 +43,11 @@ class StyleLoss:
                 loss += self._loss(input_gram_matrix, target_gram_matrix)
 
         if loss is not None:
-            loss /= len(image_features)
+            loss /= len(input_features)
         return loss
 
     @staticmethod
-    def _gram_matrix(x: torch.Tensor) -> torch.Tensor:
+    def gram_matrix(x: torch.Tensor) -> torch.Tensor:
         """
         Compute gram matrix of given input matrix.
 
