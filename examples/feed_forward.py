ab3947d155b1476633788b5b20393781f60edb6a
Kacper <kacperkrzeminski123@gmail.com>
2022-04-18 16:59:47 +0200

    -add gradient clipping -remove clamp in training step -remove batch processing(batch size was 1 anyway so no difference?) -change weights
    A visible improvement in results. However still nowhere near perfect

--- examples/feed_forward.py
+++ examples/feed_forward.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     model = FeedForward(style_image)
     datamodule = COCO128DataModule()
 
-    trainer = pl.Trainer(max_epochs=100, log_every_n_steps=10, gpus=1, gradient_clip_val=1e-5, gradient_clip_algorithm="value")
+    trainer = pl.Trainer(max_epochs=100, log_every_n_steps=10, gpus=1, gradient_clip_val=1e-6, gradient_clip_algorithm="value")
     trainer.fit(model, datamodule)
